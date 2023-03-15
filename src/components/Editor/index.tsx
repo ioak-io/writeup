cc01ff629c6d83fc0ecd5d3f86e6e3d9a661f915
Arunkumar Selvaraj <arun@ioak.io>
2023-03-15 09:41:20 +0530

    Base version with prosemirror

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,113 +1,51 @@
-import { AutoLinkPlugin } from '@lexical/react/LexicalAutoLinkPlugin';
-import { CheckListPlugin } from '@lexical/react/LexicalCheckListPlugin';
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
-import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
-import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
-import { ListPlugin } from '@lexical/react/LexicalListPlugin';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { TablePlugin } from '@lexical/react/LexicalTablePlugin';
 import * as React from 'react';
-import { useEffect, useState } from 'react';
-import Editor from '../../Editor';
-import PlaygroundNodes from '../../nodes/PlaygroundNodes';
-import ActionsPlugin from '../../plugins/ActionsPlugin';
-import EmojiPickerPlugin from '../../plugins/EmojiPickerPlugin';
-import EmojisPlugin from '../../plugins/EmojisPlugin';
-import ImagesPlugin from '../../plugins/ImagesPlugin';
-import ToolbarPlugin from '../../plugins/ToolbarPlugin';
-import PlaygroundEditorTheme from '../../themes/PlaygroundEditorTheme';
+import { schema } from "prosemirror-schema-basic"
+import { EditorState } from "prosemirror-state"
+import { EditorView } from "prosemirror-view"
+import { undo, redo, history } from "prosemirror-history"
+import { keymap } from "prosemirror-keymap"
+import { baseKeymap } from "prosemirror-commands"
+import { Plugin } from "prosemirror-state"
+import { dropCursor } from "prosemirror-dropcursor"
+import { gapCursor } from "prosemirror-gapcursor"
+import { menuBar, MenuItem } from "prosemirror-menu"
+
+import { buildMenuItems } from "../../core/menu"
+import { buildKeymap } from "../../core/keymap"
+import { buildInputRules } from "../../core/inputrules"
+
 import './style.css';
 import './editor.css';
-import FloatingTextFormatToolbarPlugin from '../../plugins/FloatingTextFormatToolbarPlugin';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import YouTubePlugin from '../../plugins/YouTubePlugin';
-import PollPlugin from '../../plugins/PollPlugin';
-import TwitterPlugin from '../../plugins/TwitterPlugin';
-import FigmaPlugin from '../../plugins/FigmaPlugin';
-import ClickableLinkPlugin from '../../plugins/ClickableLinkPlugin';
-import { HorizontalRulePlugin } from '@lexical/react/LexicalHorizontalRulePlugin';
-import EquationsPlugin from '../../plugins/EquationsPlugin';
-import ExcalidrawPlugin from '../../plugins/ExcalidrawPlugin';
-import TabFocusPlugin from '../../plugins/TabFocusPlugin';
-import { TabIndentationPlugin } from '@lexical/react/LexicalTabIndentationPlugin';
-import CollapsiblePlugin from '../../plugins/CollapsiblePlugin';
-import DragDropPaste from '../../plugins/DragDropPastePlugin';
-import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
-import { ClearEditorPlugin } from '@lexical/react/LexicalClearEditorPlugin';
-import ComponentPickerMenuPlugin from '../../plugins/ComponentPickerPlugin';
-import AutoEmbedPlugin from '../../plugins/AutoEmbedPlugin';
-import NewMentionsPlugin from '../../plugins/MentionsPlugin';
-import { HashtagPlugin } from '@lexical/react/LexicalHashtagPlugin';
-import KeywordsPlugin from '../../plugins/KeywordsPlugin';
-import SpeechToTextPlugin from '../../plugins/SpeechToTextPlugin';
-import CommentPlugin from '../../plugins/CommentPlugin';
-import TableCellResizerPlugin from '../../plugins/TableCellResizer';
-import ListMaxIndentLevelPlugin from '../../plugins/ListMaxIndentLevelPlugin';
-import CodeHighlightPlugin from '../../plugins/CodeHighlightPlugin';
-import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin';
 
-const EditorWrapper = () => {
+export default function EditorWrapper(): JSX.Element {
+  const documentRef = React.useRef<any>();
+  React.useEffect(() => {
+    const plugins = [
+      history(),
+      keymap({ "Mod-z": undo, "Mod-y": redo }),
+      keymap(baseKeymap)
+    ];
+    plugins.push(menuBar({
+      floating: false,
+      content: buildMenuItems(schema).fullMenu
+    }))
+    let state = EditorState.create({
+      schema, plugins
+    });
 
-  const initialConfig = {
-    namespace: 'Playground',
-    onError: (error: Error) => {
-      throw error;
-    },
-    theme: PlaygroundEditorTheme,
-    nodes: [...PlaygroundNodes],
-  };
+    let view = new EditorView(documentRef.current, {
+      state, dispatchTransaction(transaction) {
+        console.log("Document size went from", transaction.before.content.size,
+          "to", transaction.doc.content.size)
+        let newState = view.state.apply(transaction)
+        view.updateState(newState)
+      }
+    });
+
+    console.log(view);
+  }, [documentRef]);
 
   return (
-    <div>
-      test
-      <LexicalComposer initialConfig={initialConfig}>
-        <div className="editor-shell">
-          {/* <ToolbarPlugin />
-          <div className="editor-container">
-            <RichTextPlugin
-              placeholder={<></>}
-              contentEditable={<ContentEditable className='editor' />}
-              ErrorBoundary={LexicalErrorBoundary} />
-            <DragDropPaste />
-            <AutoFocusPlugin />
-            <ClearEditorPlugin />
-            <ComponentPickerMenuPlugin />
-            <EmojiPickerPlugin />
-            <AutoEmbedPlugin />
-            <NewMentionsPlugin />
-            <EmojisPlugin />
-            <HashtagPlugin />
-            <KeywordsPlugin />
-            <SpeechToTextPlugin />
-            <HistoryPlugin />
-
-            <MarkdownShortcutPlugin />
-            <CodeHighlightPlugin />
-            <ListPlugin />
-            <CheckListPlugin />
-            <ListMaxIndentLevelPlugin maxDepth={7} />
-            <TablePlugin />
-            <TableCellResizerPlugin />
-            <ImagesPlugin />
-            <LinkPlugin />
-            <PollPlugin />
-            <TwitterPlugin />
-            <YouTubePlugin />
-            <FigmaPlugin />
-            <ClickableLinkPlugin />
-            <HorizontalRulePlugin />
-            <EquationsPlugin />
-            <ExcalidrawPlugin />
-            <TabFocusPlugin />
-            <TabIndentationPlugin />
-            <CollapsiblePlugin /> */}
-          {/* </div> */}
-          <Editor />
-        </div>
-      </LexicalComposer>
-    </div>
+    <div ref={documentRef}>Editor</div>
   );
 }
-
-export default EditorWrapper;
